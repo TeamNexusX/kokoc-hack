61b26f3a34e7c9158f827d4a9e84120d968eea9f
keyready <vip.korchak@list.ru>
2023-08-31 10:04:20 +0300

    master

--- server/config/db.connect.js
+++
@@ -1,7 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-module.exports = new Sequelize('cloud-drive', 'keyready', 'userSQL', {
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-});
--- src/features/AuthByUsername/model/types/loginSchema.ts
+++
@@ -1,9 +0,0 @@
-export interface LoginSchema {
-    login: string;
-    password: string;
-    firstname: string;
-    lastname: string;
-    sex: string;
-    isLoading: boolean;
-    error?: string;
-}
