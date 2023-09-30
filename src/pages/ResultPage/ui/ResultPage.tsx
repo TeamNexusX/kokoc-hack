b24ac3bdfbb447e6c4a4b9d7c625d015f29d496f
keyready <vip.korchak@list.ru>
2023-10-01 02:36:55 +0300

    добавил кнопочку перехода на сайт на result page

--- src/pages/ResultPage/ui/ResultPage.tsx
+++ src/pages/ResultPage/ui/ResultPage.tsx
@@ -1,15 +1,17 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Page } from 'widgets/Page/Page';
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
 import { Card } from 'shared/UI/Card';
 import { Text } from 'shared/UI/Text';
 import { HStack, VStack } from 'shared/UI/Stack';
+import LinkIcon from 'shared/assets/icons/link.svg';
 import { Button } from 'shared/UI/Button';
 import { Disclosure } from 'shared/UI/Disclosure';
 import { useSelector } from 'react-redux';
 import { linkResult } from 'pages/MainPage';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import { useNavigate } from 'react-router-dom';
+import { Icon } from 'shared/UI/Icon/Icon';
 import classes from './ResultPage.module.scss';
 
 interface ResultPageProps {
@@ -29,14 +31,24 @@ const ResultPage = memo((props: ResultPageProps) => {
         return <div />;
     }
 
+    const handleOriginLinkClick = useCallback(() => {
+        window.open(link, '_blank');
+    }, [link]);
+
     return (
         <Page className={classNames(classes.ResultPage, {}, [className])}>
-            <VStack gap="32" maxW align="center"
-justify="start">
+            <VStack gap="32" maxW align="center" justify="start">
                 <Card>
                     <HStack maxW justify="between" align="center">
                         <Text className={classes.title} text="Ссылка" />
-                        <Text className={classes.text} text={link} />
+                        <HStack gap="8">
+                            <Text className={classes.text} text={link} />
+                            <Button variant="clear" onClick={handleOriginLinkClick}>
+                                <HStack>
+                                    <Icon Svg={LinkIcon} className={classes.linkIcon} />
+                                </HStack>
+                            </Button>
+                        </HStack>
                     </HStack>
                 </Card>
                 <Card>
